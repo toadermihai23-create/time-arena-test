e7a079d3afa2ba9e1cfb20ff426b569b70105696
toadermihai23-create <toadermihai23@gmail.com>
2025-12-16 10:59:15 +0200

    Update service worker cache version and assets
    Updated cache version and removed unused assets.

--- sw.js
+++ sw.js
@@ -1,24 +1,15 @@
-const CACHE = "timearena-demo-v1.0.0";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./missions.js",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE = "timearena-demo-v1.0.3";
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-});
-
-self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./missions.js",
+        "./app.js"
+      ])
     )
   );
 });
